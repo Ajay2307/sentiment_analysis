6294b4450a8cecae26c832cba2fc3ea3eb0f04b2
Ajay <ajay.prodevans@gmail.com>
2019-12-04 11:00:06 +0000

    changed title

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ colors = {
 }
 
 app.layout = html.Div(children=[
-    html.H1(children='Welcome to iVentura - Data Detective',style={
+    html.H1(children='Welcome to iVentura Platform',style={
         'textAlign': 'center',
         'color': colors['text']}),
     dcc.Graph(id='project1',
